3f72ed66f86c46947fbb7d4f3f5d7a18575c26b2
mozo <mafizchowdhury006@gmail.com>
2026-03-23 12:17:08 +0600

    finish th game part- 1

---
+++ checkers/board.py
@@ -0,0 +1,58 @@
+import pygame
+from checkers.constants import BLACK, COLS, RED, ROWS, SQUARE_SIZE, WHITE
+from checkers.piece import piece
+
+class Board:
+    def __init__(self):
+        self.board = [[]]
+        self.red_left = self.white_left = 12
+        self.red_kings = self.white_kings = 0
+        self.create_board()
+
+    def draw_squares(self, win):
+        win.fill(BLACK)
+        for row in range(8):
+            for col in range(row % 2, 8, 2):
+                pygame.draw.rect(win, (RED), (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+    
+    def move(self, piece, row, col):
+        self.board[piece.row][piece.col], self.board[row][col] = self.board[row][col], self.board[piece.row][piece.col]
+        
+        piece.move(row, col)
+        
+
+        if row == ROWS or row == 0:
+            piece.make_king()
+            if piece.color == WHITE:
+                self.white_kings += 1
+            else:
+                self.red_kings += 1
+
+    def get_piece(self, row, col):
+        return self.board[row][col]
+    
+
+    def create_board(self):
+        for row in range(ROWS):
+            self.board.append([])
+            for col in range(COLS):
+                if col % 2 == ((row + 1) % 2):
+                    if row < 3:
+                        self.board[row].append(piece(row, col, WHITE))
+                    elif row > 4:
+                        self.board[row].append(piece(row, col, RED))
+                    else:
+                        self.board[row].append(0)
+                else:
+                    self.board[row].append(0)
+    
+    def draw(self, win):
+        self.draw_squares(win)
+        for row in range(ROWS):
+            for col in range(COLS):
+                piece = self.board[row][col]
+                if piece != 0:
+                    piece.draw(win)
+        
+    
+    
